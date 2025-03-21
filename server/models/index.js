444c34ffd2a9ec043c782ee5a20560eac75a5f25
kmexnx <46535537+kmexnx@users.noreply.github.com>
2025-03-21 12:00:02 -0600

    Add models index file

---
+++ server/models/index.js
@@ -0,0 +1,11 @@
+const Guest = require('./Guest');
+const Room = require('./Room');
+const Booking = require('./Booking');
+const Staff = require('./Staff');
+
+module.exports = {
+  Guest,
+  Room,
+  Booking,
+  Staff
+};
