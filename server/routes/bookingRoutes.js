511123261e9d79e146de3dbc0e8e2aa56781ea49
kmexnx <46535537+kmexnx@users.noreply.github.com>
2025-03-21 12:02:35 -0600

    Add booking routes

---
+++ server/routes/bookingRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const bookingController = require('../controllers/bookingController');
+
+// Routes for /api/bookings
+router.get('/', bookingController.getAllBookings);
+router.get('/:id', bookingController.getBookingById);
+router.get('/guest/:guestId', bookingController.getBookingsByGuest);
+router.post('/', bookingController.createBooking);
+router.put('/:id', bookingController.updateBooking);
+router.delete('/:id', bookingController.deleteBooking);
+
+module.exports = router;
